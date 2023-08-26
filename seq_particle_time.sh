39a835543c00478520e022bf817aec20ea7024df
Zimin <chen.zimin@hotmail.com>
2023-08-26 13:34:08 +0200

    Add num_layer experiment as well

--- seq_particle_time.sh
+++ seq_particle_time.sh
@@ -12,3 +12,15 @@ for i in {1..6}; do
         srun -n 1 ./energy_storms_seq 30000 "${test_files}" >> "${output_file}"
     done
 done
+
+directory="result/num_layer"
+test_files="test_files/test_02_a30k_p20k_w1"
+num_layer=1
+mkdir -p "${directory}"
+for i in {1..20}; do
+    output_file="${directory}/seq_a{$num_layer}_w1_time.txt"
+    for times in {1..10}; do
+        srun -n 1 ./energy_storms_seq ${num_layer} "${test_files}" >> "${output_file}"
+    done
+    num_layer=$(( 2*num_layer ))
+done

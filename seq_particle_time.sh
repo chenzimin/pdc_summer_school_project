13955431743d9896220f26d13380c349dc746301
Zimin <chen.zimin@hotmail.com>
2023-08-26 13:21:15 +0200

    Update script

--- seq_particle_time.sh
+++ seq_particle_time.sh
@@ -1,38 +1,14 @@
-for run in {1..10}; do
-  srun -n 1 ./energy_storms_seq 30000 test_files/test_02_a30k_p20k_w1 >> seq_a30k_w1_time.txt
-done
+#!/bin/bash
 
-for run in {1..10}; do
-  srun -n 1 ./energy_storms_seq 30000 test_files/test_02_a30k_p20k_w1 \
-                                      test_files/test_02_a30k_p20k_w2 >> seq_a30k_w2_time.txt
-done
+directory="result/num_particles"
+filename_prefix="test_02_a30k_p20k_w"
+test_files=""
+mkdir -p "${directory}"
 
-for run in {1..10}; do
-  srun -n 1 ./energy_storms_seq 30000 test_files/test_02_a30k_p20k_w1 \
-                                      test_files/test_02_a30k_p20k_w2 \
-                                      test_files/test_02_a30k_p20k_w3 >> seq_a30k_w3_time.txt
-done
-
-for run in {1..10}; do
-  srun -n 1 ./energy_storms_seq 30000 test_files/test_02_a30k_p20k_w1 \
-                                      test_files/test_02_a30k_p20k_w2 \
-                                      test_files/test_02_a30k_p20k_w3 \
-                                      test_files/test_02_a30k_p20k_w4 >> seq_a30k_w4_time.txt
-done
-
-for run in {1..10}; do
-  srun -n 1 ./energy_storms_seq 30000 test_files/test_02_a30k_p20k_w1 \
-                                      test_files/test_02_a30k_p20k_w2 \
-                                      test_files/test_02_a30k_p20k_w3 \
-                                      test_files/test_02_a30k_p20k_w4 \
-                                      test_files/test_02_a30k_p20k_w5 >> seq_a30k_w5_time.txt
-done
-
-for run in {1..10}; do
-  srun -n 1 ./energy_storms_seq 30000 test_files/test_02_a30k_p20k_w1 \
-                                      test_files/test_02_a30k_p20k_w2 \
-                                      test_files/test_02_a30k_p20k_w3 \
-                                      test_files/test_02_a30k_p20k_w4 \
-                                      test_files/test_02_a30k_p20k_w5 \
-                                      test_files/test_02_a30k_p20k_w6 >> seq_a30k_w6_time.txt
+for i in {1..6}; do
+    test_files="${test_files} ${filename_prefix}${i}"
+    output_file="${directory}/seq_a30k_w${i}_time.txt"
+    for times in {1..10}; do
+        srun -n 1 ./energy_storms_seq "${test_files}" >> "${output_file}"
+    done
 done

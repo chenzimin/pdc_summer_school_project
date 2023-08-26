5e7498abfda57a1c19293d9f640fe08c1bb5c9dc
Zimin <chen.zimin@hotmail.com>
2023-08-26 13:25:07 +0200

    Add testfile prefix

--- seq_particle_time.sh
+++ seq_particle_time.sh
@@ -6,7 +6,7 @@ test_files=""
 mkdir -p "${directory}"
 
 for i in {1..6}; do
-    test_files="${test_files} ${filename_prefix}${i}"
+    test_files="${test_files} test_files${filename_prefix}${i}"
     output_file="${directory}/seq_a30k_w${i}_time.txt"
     for times in {1..10}; do
         srun -n 1 ./energy_storms_seq 30000 "${test_files}" >> "${output_file}"

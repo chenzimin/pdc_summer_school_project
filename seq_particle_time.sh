bad59dd154f818683098f833c1bf00d3d3be45e3
Zimin <chen.zimin@hotmail.com>
2023-08-26 14:01:40 +0200

    Add the correct bash script

--- seq_particle_time.sh
+++ seq_particle_time.sh
@@ -11,7 +11,7 @@ for i in {1..6}; do
     test_files="${test_files} ${curent_dir}/test_files/${filename_prefix}${i}"
     output_file="${directory}/seq_a30k_w${i}_time.txt"
     for times in {1..10}; do
-        srun -n 1 "${curent_dir}/energy_storms_seq" 30000 "${test_files}" >> "${output_file}"
+        srun -n 1 "${curent_dir}/energy_storms_seq" 30000 ${test_files} >> "${output_file}"
     done
 done
 
@@ -20,9 +20,9 @@ test_files="${curent_dir}/test_files/test_02_a30k_p20k_w1"
 num_layer=1
 mkdir -p "${directory}"
 for i in {1..20}; do
-    output_file="${directory}/seq_a{$num_layer}_w1_time.txt"
+    output_file="${directory}/seq_a"${num_layer}"_w1_time.txt"
     for times in {1..10}; do
-        srun -n 1 "${curent_dir}/energy_storms_seq" ${num_layer} "${test_files}" >> "${output_file}"
+        srun -n 1 "${curent_dir}/energy_storms_seq" ${num_layer} ${test_files} >> "${output_file}"
     done
     num_layer=$(( 2*num_layer ))
 done
